c293da6e07771af49ec3ed9119c424e19b17efa0
손기연 <sonky740@minwise.co.kr>
2022-06-28 15:07:21 +0900

    59~~

--- 34-compiler/src/app.ts
+++ 34-compiler/src/app.ts
@@ -1,7 +1,16 @@
 const button = document.querySelector('button');
 
 const clickHandler = (message: string) => {
-    console.log(`Clicked! ${message}`);
-}
+  console.log(`Clicked! ${message}`);
+};
 
-button?.addEventListener('click', clickHandler.bind(null, 'Complete'))
+button?.addEventListener('click', clickHandler.bind(null, 'Complete'));
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
