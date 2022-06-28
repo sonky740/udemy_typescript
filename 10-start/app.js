80c182b2143df35e0d80413774acf8cfbf795a88
손기연 <sonky740@minwise.co.kr>
2022-06-28 12:07:01 +0900

    48~~

--- 10-start/app.js
+++ 10-start/app.js
@@ -1,21 +1,12 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    //   if (resultConversion === 'as-number') {
-    //     return +result;
-    //   } else {
-    //     return result.toString();
-    //   }
-    return result;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+var result = generateError('An error occurred!', 500);
+console.log(result);
--- 10-start/app.ts
+++ 10-start/app.ts
@@ -1,33 +1,14 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
+let userInput: unknown;
+let userName: string;
 
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result: Combinable;
-  if (
-    (typeof input1 === 'number' && typeof input2 === 'number') ||
-    resultConversion === 'as-number'
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  //   if (resultConversion === 'as-number') {
-  //     return +result;
-  //   } else {
-  //     return result.toString();
-  //   }
-  return result;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+generateError('An error occurred!', 500);
