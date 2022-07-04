606b040ca1d0788030082c1e7b3deff450738ef8
손기연 <sonky740@minwise.co.kr>
2022-07-04 16:15:36 +0900

    153~~~

---
+++ 120-dragdrop/dist/decorators/autobind.js
@@ -0,0 +1,13 @@
+export const Autobind = (_, _2, descriptor) => {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+};
+//# sourceMappingURL=autobind.js.map
